48b1a300662f89b689b99a0df3c7c6f22932504f
Jeong Hae-seon <haeseon0363@naver.com>
2017-10-08 18:25:30 +0900

    finished 10.08

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,30 @@
 #include "utils.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+  char newCom[8096];
+	strcpy(newCom,command);
+	*argv=(char **)malloc(sizeof(char*)*16);
+	int count=0;
+	char *token = strtok(newCom," \n\t");
+	
+	while(token!=NULL){
+		*(*argv+count)=(char*)malloc(sizeof(char)*(strlen(token)));
+		strcpy(*(*argv+count),token);
+		count++;
+
+		token=strtok(NULL," \n\t");
+	}
+        
+	if (count==0)
+	{
+		*(*argv+count)=(char *)malloc(sizeof(char));
+		strcpy(*(*argv+count),"");
+		count++;
+	}
+
+       *argc=count;
 }
